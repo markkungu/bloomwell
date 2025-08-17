e4c1662fe86abf2564ee3531ce852a739ae1db40
yourboykungu <badusvanmark@gmail.com>
2025-08-17 22:37:12 +0300

    setup

---
+++ src/pages/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+  <div class="p-8 text-center">
+    <h1 class="text-3xl font-bold text-blue-600">Home Page 🏠</h1>
+    <p class="mt-4">Welcome to the homepage</p>
+  </div>
+</template>
+
+
